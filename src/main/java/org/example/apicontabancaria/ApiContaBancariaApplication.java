8f6687c01b9046a4c969e80b0774ba38c7e5b61e
vinicius-rebelatto <vinicius-rebelatto@hotmail.com>
2024-10-26 14:35:44 -0300

    First Commit

---
+++ src/main/java/org/example/apicontabancaria/ApiContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package org.example.apicontabancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/apicontabancaria/ApiContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apicontabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
